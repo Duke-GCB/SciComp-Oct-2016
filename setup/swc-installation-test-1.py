0333f1b49877e7163315bcc620301e3f71809430
Dan Leehr <dan.leehr@duke.edu>
2015-10-01 13:24:52 -0400

    Change python test to require 2.7

--- setup/swc-installation-test-1.py
+++ setup/swc-installation-test-1.py
@@ -39,7 +39,7 @@ __version__ = '0.2'
 
 
 def check():
-    if _sys.version_info < (3, 3):
+    if _sys.version_info < (2, 7):
         print('check for Python version (python):')
         print('outdated version of Python: ' + _sys.version)
         return False
@@ -51,6 +51,6 @@ if __name__ == '__main__':
         print('Passed')
     else:
         print('Failed')
-        print('Install a current version of Python 3!')
+        print('Install a current version of Python 2!')
         print('http://continuum.io/downloads')
         _sys.exit(1)
